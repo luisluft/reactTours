e11b492a8be0afa5476027b51fd3d1d0f67f1de8
luisluft <luberluft@gmail.com>
2021-09-14 08:21:31 -0400

    implement the remove tour and refresh features

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
   const fetchTours = async () => {
     setLoading(true);
 
@@ -34,9 +39,21 @@ function App() {
     );
   }
 
+  if (tours.length === 0)
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            Search more tours
+          </button>
+        </div>
+      </main>
+    );
+
   return (
     <main>
-      <Tours tours={tours}></Tours>
+      <Tours tours={tours} removeTour={removeTour}></Tours>
     </main>
   );
 }

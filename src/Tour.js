b456d53fc4ebe1787c0d712ca8492619ee917475
luisluft <luberluft@gmail.com>
2021-09-14 08:12:36 -0400

    implement show more and show less functionality

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
 const Tour = ({ id, name, info, image, price }) => {
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
@@ -9,7 +11,12 @@ const Tour = ({ id, name, info, image, price }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
-        <p>{info}</p>
+        <p>
+          {readMore ? info : `${info.substring(0, 100)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? "show less" : "show more"}
+          </button>
+        </p>
         <button className="delete-btn">not interested</button>
       </footer>
     </article>
